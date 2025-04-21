e7748d7534356d47204cd9a036afb7d7765bd635
kaic-cesar <kaic_cesr@hotmail.com>
2025-04-21 17:47:20 -0300

    Variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,20 @@
+var name = "Kaic";
+console.log(name);
+
+name = "Kaic Cesar";
+console.log(name);
+
+
+let age = 23;
+console.log(age); 
+
+age = 24;
+console.log(age); 
+
+
+const pi = 3.14
+console.log(pi)
+
+pi = 5;
+console.log(pi)
+// Const é uma variável imutável, não é possível fazer a alteração a não ser na onde foi declarada
