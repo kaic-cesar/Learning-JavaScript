9062f5111cdc9f9e372824997551294dadb4aa60
kaic-cesar <kaic_cesr@hotmail.com>
2025-04-21 18:15:44 -0300

    Funcões parametrizadas

--- functions/script.js
+++ functions/script.js
@@ -24,3 +24,25 @@ function sair() {
     text.innerHTML = "Você saiu"
 }
 
+
+
+// FUNÇÕES PARAMETRIZADAS
+
+var area = document.getElementById('area');
+var result = document.createElement("h1");
+area.appendChild(result);
+
+function mediaAluno(n1, n2) {
+    var media = (n1 + n2) / 2
+
+    if(media >= 6.0) {
+        result.innerHTML = "Você foi aprovado! Sua média foi de " + media;
+    } else {
+        result.innerHTML = "Você foi reprovado Sua média foi de " + media;
+    }
+}
+
+function aluno(nome, curso) {
+    var mensagem = `Seja bem vindo ${nome} ao seu curso de ${curso}!` 
+    console.log(mensagem);
+}
