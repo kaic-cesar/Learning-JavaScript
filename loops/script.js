ca7ea6b578da4012138213284c1d5220d2cd41aa
kaic-cesar <kaic_cesar@hotmail.com>
2025-04-22 20:27:17 -0300

    Loops - For e While

---
+++ loops/script.js
@@ -0,0 +1,22 @@
+
+// WHILE -> Enquanto
+
+document.writeln("UTILIZANDO WHILE:<br>")
+
+var x = 0;
+
+while(x <= 10) {
+    console.log(x);
+    document.writeln("<br> O valor de X é: " + x);
+
+    // Aumenta o valor de X
+    x++;
+}
+
+
+// FOR -> Para 
+document.writeln("<br><br><br>UTILIZANDO FOR:<br>")
+
+for(a = 0; a < 10; a++) {
+    document.writeln("<br> O valor de A é: " + a);
+}
