50f41a53bd484f682997a9681d4c3b5e6852842a
Kaic Cesar <kaic_cesar@hotmail.com>
2025-07-02 20:16:37 -0300

    Loops - While e For

---
+++ Loops/script.js
@@ -0,0 +1,16 @@
+// While
+document.writeln("While:");
+
+let i = 0;
+while(i <= 10) {
+    document.writeln(`<br>${i}`);
+    i++;
+}
+
+
+// For
+document.writeln("<br><br>For");
+
+for(i = 0; i <= 10; i++) {
+    document.writeln(`<br>${i}`)
+}
